f6b73c811eeac2d13440d9114783f548d216d1b6
Opilane TTHK <opilane@tthk.ee>
2021-12-08 10:34:34 +0200

    bitcoincalculator

---
+++ BitcoinCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace BitcoinCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitcoinRate currentBitcoin = GetRates();
+            Console.WriteLine($"current rate: {currentBitcoin.bpi.USD.code} {currentBitcoin.bpi.USD.rate_float}");
+            Console.WriteLine($"{currentBitcoin.disclaimer}");
+
+
+        }
+
+        public static BitcoinRate GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitcoinRate bitcoinData;
+            using(var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoinData = JsonConvert.DeserializeObject<BitcoinRate>(response);
+            }
+
+            return bitcoinData;
+
+        }
+    } 
+}
